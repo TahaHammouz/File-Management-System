ce8c8815b99ae4e6261804b813fa5fbb6626fee4
Taha Hammouz <tahahammouz2k01@gmail.com>
2022-12-22 16:05:29 +0200

    Logger Component

--- src/main/java/Logger/Logger.java
+++ src/main/java/Logger/Logger.java
@@ -1,4 +1,48 @@
 package main.java.Logger;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Logger {
+
+    private static Logger instance;
+    static ArrayList<String> logsArray = new ArrayList<String>(5);
+
+    private Logger() {}
+
+    public static Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public static void logInfo(String message) {
+        logsArray.add(java.time.LocalDateTime.now() + " [INFO] " + message);
+    }
+
+    public static void logDebug(String message) {
+        logsArray.add(java.time.LocalDateTime.now() + " [Debug] " + message);
+    }
+
+    public static void logWarning(String message) {
+        logsArray.add(java.time.LocalDateTime.now() + " [Warn] " + message);
+    }
+
+    public static void logError(String message) {
+        logsArray.add(java.time.LocalDateTime.now() + " [Error] " + message);
+    }
+
+    public static void SaveLogs() {
+        try {
+            FileWriter out = new FileWriter("./logs.txt", true);
+            for (int i = 0; i < logsArray.size(); i++) out.write(
+                    logsArray.get(i) + "\n"
+            );
+
+            out.close();
+        } catch (IOException ex) {
+            System.out.println("IOException: " + ex.getMessage());
+        }
+    }
 }
