bd897bb3af27c7f03196c0485ba4ecc6c73ebc0b
abedalaziz-kharobi <abedmk37@gail.com>
2022-12-23 17:53:34 +0200

    constants variables

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -2,5 +2,10 @@ package Constants;
 
 public class Constants {
     public static String SQLiteURL = "jdbc:sqlite:FilesRepository.db";
+    public static final String ALGORITHM = "AES";
+    public static final String KEY_DERIVATION_ALGORITHM = "SHA-256";
+    public static final int KEY_LENGTH = 32;
+    public static final String PASSWORD = "teamE";
+    public static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
 
 }
--- src/main/java/Crypto/Encryption.java
+++ src/main/java/Crypto/Encryption.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package Crypto;
 
 import java.security.MessageDigest;
@@ -10,22 +5,16 @@ import java.util.Arrays;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import static Constants.Constants.*;
 
 public class Encryption {
-    private static final String ALGORITHM = "AES";
-    private static final String KEY_DERIVATION_ALGORITHM = "SHA-256";
-    private static final int KEY_LENGTH = 32;
-    private static final String PASSWORD = "teamE";
-
-    public Encryption() {
-    }
 
     public static String encrypt(String fileName) throws Exception {
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] key = digest.digest("teamE".getBytes());
-        key = Arrays.copyOf(key, 32);
-        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        MessageDigest digest = MessageDigest.getInstance(KEY_DERIVATION_ALGORITHM);
+        byte[] key = digest.digest(PASSWORD.getBytes());
+        key = Arrays.copyOf(key, KEY_LENGTH);
+        Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+        SecretKeySpec keySpec = new SecretKeySpec(key, ALGORITHM);
         cipher.init(1, keySpec);
         byte[] encryptedFileName = cipher.doFinal(fileName.getBytes());
         return Base64.getEncoder().encodeToString(encryptedFileName);
