d639be39fadf42679f62311a5e7df6c7b1fd6565
malak-musa <malakmousa027@gmail.com>
2022-12-23 23:35:25 +0200

    Export File Feature

--- src/main/java/Controller/Get.java
+++ src/main/java/Controller/Get.java
@@ -1,4 +1,19 @@
 package Controller;
 
+import java.util.Scanner;
+import Crypto.Encryption;
+import static Connection.Database.retrieveFile;
+
 public class Get {
+
+    public static void exportFile() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter File Name (with extension) to Export: ");
+        String fileName = scanner.nextLine();
+
+        retrieveFile(Encryption.encrypt(fileName));
+
+        //System.out.println("Done, the file is exported.");
+        //System.out.println("The file is not exist.");
+    }
 }
