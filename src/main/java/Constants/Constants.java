374ce4a255030a7fc08a767101febf51762b04d3
abedalaziz-kharobi <abedmk37@gail.com>
2022-12-24 12:32:08 +0200

    fixes for import feature

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -7,6 +7,11 @@ public class Constants {
     public static final int KEY_LENGTH = 32;
     public static final String PASSWORD = "teamE";
     public static final String TRANSFORMATION = "AES/ECB/PKCS5Padding";
+    public static final int KILOBYTE = 1024;
+    public static final int MEGABYTE = (1024*1024);
+    public static final String SMALL = "small";
+    public static final String MEDIUM = "medium";
+    public static final String LARGE = "large";
 
 
 }
